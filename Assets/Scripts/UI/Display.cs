9555081408ec253b1f2f119a97d48be2fc887fc9
Bulaev Alexey <aracool1982@gmail.com>
2020-06-19 11:43:23 +0700

    изменен Display

--- Assets/Scripts/UI/Display.cs
+++ Assets/Scripts/UI/Display.cs
@@ -4,22 +4,16 @@ using TMPro;
 public class Display : MonoBehaviour
 {
     [SerializeField] private Player _player;
-    [SerializeField] private TypeResource _type;
+    [SerializeField] private TypeResource _typeResource;
     [SerializeField] private TMP_Text _text;
     
     private void OnChangeResource(IResourceble resource)
     {
-        if (_type == resource.Type)
+        if (_typeResource == resource.Type)
             _text.text = resource.Type.ToString() + ": " + resource.Amount.ToString();
     }
 
-    private void OnEnable()
-    {
-        _player.ChangeResource += OnChangeResource;
-    }
+    private void OnEnable() => _player.ChangeResource += OnChangeResource;
 
-    private void OnDisable()
-    {
-        _player.ChangeResource += OnChangeResource;
-    }
+    private void OnDisable() => _player.ChangeResource -= OnChangeResource;
 }
